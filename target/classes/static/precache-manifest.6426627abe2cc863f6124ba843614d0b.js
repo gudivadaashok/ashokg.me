2e1f49ca32b755ec0c45f91c33321b97ef26e443
Ashok Gudivada <ashok.gudivada@eagles.oc.edu>
2019-10-21 23:56:28 -0400

    first commit

---
+++ target/classes/static/precache-manifest.6426627abe2cc863f6124ba843614d0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a8f4dc6051482ab02f4f7a79a915589",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a46151000d6363c3c30",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2daac6094298103aedaa",
+    "url": "/static/js/2.912d5308.chunk.js"
+  },
+  {
+    "revision": "4a46151000d6363c3c30",
+    "url": "/static/js/main.e2bf0a80.chunk.js"
+  },
+  {
+    "revision": "aa5dd861c3df341bfb4f",
+    "url": "/static/js/runtime-main.0fda84ed.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
---
+++ target/classes/static/precache-manifest.f51029485e897589dcb3b084a0a912dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fb051028e5c53c9fed351ead6ef3ead",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a46151000d6363c3c30",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2daac6094298103aedaa",
+    "url": "/static/js/2.912d5308.chunk.js"
+  },
+  {
+    "revision": "4a46151000d6363c3c30",
+    "url": "/static/js/main.e2bf0a80.chunk.js"
+  },
+  {
+    "revision": "aa5dd861c3df341bfb4f",
+    "url": "/static/js/runtime-main.0fda84ed.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
